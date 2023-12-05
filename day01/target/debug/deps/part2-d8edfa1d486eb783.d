8ce51e290a986fcd6aa8ca075eb7c8a6d725e75c
Jonathan Foster <fostertheweb@gmail.com>
2023-12-05 13:09:00 -0500

    Delete day01/target directory
    noob

--- day01/target/debug/deps/part2-d8edfa1d486eb783.d
+++
@@ -1,6 +0,0 @@
-/Users/jonathan/Developer/aoc/day01/target/debug/deps/part2-d8edfa1d486eb783: src/bin/part2.rs src/bin/./part1.txt
-
-/Users/jonathan/Developer/aoc/day01/target/debug/deps/part2-d8edfa1d486eb783.d: src/bin/part2.rs src/bin/./part1.txt
-
-src/bin/part2.rs:
-src/bin/./part1.txt:
--- day01/target/debug/part2.d
+++
@@ -1,1 +0,0 @@
-/Users/jonathan/Developer/aoc/day01/target/debug/part2: /Users/jonathan/Developer/aoc/day01/src/bin/part1.txt /Users/jonathan/Developer/aoc/day01/src/bin/part2.rs
